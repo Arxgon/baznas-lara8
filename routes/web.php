c0bb18202d5ced0969da57eb4374303aded05ace
Iqbal Firmansyah <fqbal3@gmail.com>
2025-01-07 13:53:47 +0700

    route default

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('display');
+});
 Route::get('/display', function () {
     return view('display');
 });
